e4f1e83b4315bdeea0c07bada29657145863ad87
nryeo <114977245+nryeo@users.noreply.github.com>
2024-12-11 17:20:15 +0900

    Update streamlit_app.py
    customer bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,53 @@
 import streamlit as st
 import qrcode
 from PIL import Image
-from io import BytesIO
+import io
 
-def generate_qr_code(data):
+ 
+def generate_qr_code(data, fill_color='black', back_color='white', box_size=10, border=4):
+    """
+    Generate a QR code with customizable parameters
+
+    :param data: Text or URL to encode in QR code
+    :param fill_color: Color of the QR code
+    :param back_color: Background color of the QR code
+    :param box_size: Size of each box in the QR code
+    :param border: Size of the border
+    :return: PIL Image of the QR code
+    """
+
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
+        box_size=box_size,
+        border=border,
     )
     qr.add_data(data)
     qr.make(fit=True)
 
-    img = qr.make_image(fill_color="black", back_color="white")
-    return img
+    qr_img = qr.make_image(fill_color=fill_color, back_color=back_color)
+    return qr_img
 
-# Streamlit App
-st.title("QR Code Generator")
+def main():
+    st.title(' QR Code Generator')    
 
-# Input text from the user
-input_text = st.text_input("Enter text to generate QR code:")
+    # Sidebar for customization
+    st.sidebar.header('QR Code Customization')
 
-if input_text:
-    # Generate QR code
-    qr_image = generate_qr_code(input_text)
+    # Input data
+    qr_data = st.text_input('Enter text or URL', placeholder='https://example.com')    
 
-    # Display the QR code
-    st.image(qr_image, caption="Generated QR Code", use_column_width=True)
+    # Color customization
+    col1, col2 = st.columns(2)
 
-    # Provide download option
-    buffered = BytesIO()
-    qr_image.save(buffered, format="PNG")
-    buffered.seek(0)
+    with col1:
+        fill_color = st.color_picker('QR Code Color', value='#000000')
 
-    st.download_button(
-        label="Download QR Code",
-        data=buffered,
-        file_name="qr_code.png",
-        mime="image/png",
-    )
+    with col2:
+        back_color = st.color_picker('Background Color', value='#FFFFFF')
+
+    
+    # Size and border
+    box_size = st.slider('QR Code Size', min_value=5, max_value=20, value=10)
+    border = st.slider('Border Size', min_value=1, max_value=10, value=4)
+    
